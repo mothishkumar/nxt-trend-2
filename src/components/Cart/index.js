e6b391594bbb974f38d8f17f45eb0a05cf5a5a12
mothish <mothishkumar@gamail.com>
2023-06-19 07:13:30 +0000

    react-js project

---
+++ src/components/Cart/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import './index.css'
+import Header from '../Header'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="cart-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        className="cart-image"
+        alt="cart"
+      />
+    </div>
+  </>
+)
+
+export default Cart
